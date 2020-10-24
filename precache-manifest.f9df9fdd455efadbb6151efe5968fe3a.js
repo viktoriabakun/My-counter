de0e8c6463ed884b9c6460abfb28d2553110a449
viktoriabakun <viktoriabakun00@gmail.com>
2020-10-24 20:17:47 +0300

    Updates

---
+++ precache-manifest.f9df9fdd455efadbb6151efe5968fe3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b9c0e81d59a057b436b8e53408bfab",
+    "url": "/github.io/My-counter/index.html"
+  },
+  {
+    "revision": "0f31bb77a3209094d45c",
+    "url": "/github.io/My-counter/static/css/main.c1d97ee7.chunk.css"
+  },
+  {
+    "revision": "8acaf64803a99f832237",
+    "url": "/github.io/My-counter/static/js/2.19041128.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/github.io/My-counter/static/js/2.19041128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f31bb77a3209094d45c",
+    "url": "/github.io/My-counter/static/js/main.11826c86.chunk.js"
+  },
+  {
+    "revision": "e4d140257f4f7a4c55db",
+    "url": "/github.io/My-counter/static/js/runtime-main.b0bdc172.js"
+  }
+]);
